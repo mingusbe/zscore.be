fb6d2f263c3a9f52145d3c3fc72d09620b8fd21a
mingusbe <56322921+mingusbe@users.noreply.github.com>
2019-10-08 23:19:32 +0200

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'zscore';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/sw.js',
+  '/js/main.js',
+  '/js/lmsfuns.js',
+  '/images/logo/kuleuven.svg',
+  '/images/logo/vwvj.svg'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
